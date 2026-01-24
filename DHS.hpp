4bcc3c5dcea402b32c61426c5ff1e893111ee5fe
Matthew Bergin <mdb326@callisto.cse.lehigh.edu>
2026-01-24 11:24:04 -0500

    basic hash map I think?

---
+++ DHS.hpp
@@ -0,0 +1,36 @@
+#include <unordered_map>
+#include <iostream>
+//use bucket interface for locks?
+
+template <typename T>
+class DHS {
+    public:
+        DHS();
+        T get(int key);
+        bool put(int key, T val);
+
+    private:
+        std::unordered_map<int, T> m;
+
+};
+
+template <typename T>
+DHS<T>::DHS(){
+    
+}
+
+template <typename T>
+T DHS<T>::get(int key){
+    if (m.contains(key)){
+        return m[key];
+    }
+    return NULL;
+}
+template <typename T>
+bool DHS<T>::put(int key, T val){
+    if (m.contains(key)){
+        return false;
+    }
+    m[key] = val;
+    return true;
+}
