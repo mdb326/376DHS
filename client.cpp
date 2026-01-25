6ae19be1e4634e6cd647388b9edf2f1594324a8d
Matthew Bergin <mdb326@eris.cse.lehigh.edu>
2026-01-25 16:27:08 -0500

    Get results for get

--- client.cpp
+++ client.cpp
@@ -2,10 +2,12 @@
 // socket programming
 #include <cstring>
 #include <iostream>
+#include <string>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <random>
 
 int get_val(int key, std::string serverIP, int serverPort){
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,8 +36,14 @@ int get_val(int key, std::string serverIP, int serverPort){
     std::cout << "Message from server: " << buffer
                 << std::endl;
 
+    
     // closing socket
     close(clientSocket);
+
+    if (buffer[0] == '0'){
+        return NULL;
+    }
+    return std::stoi(std::string(&buffer[1]));
 }
 int put_val(int key, int val, std::string serverIP, int serverPort){
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -67,19 +75,50 @@ int put_val(int key, int val, std::string serverIP, int serverPort){
     // closing socket
     close(clientSocket);
 }
+int generateRandomInteger(int min, int max) {
+    thread_local static std::random_device rd; // creates random device (unique to each thread to prevent race cons) (static to avoid reinitialization)
+    thread_local static std::mt19937 gen(rd());  // Seeding the RNG (unique to each thread to prevent race cons) (static to avoid reinitialization)
+    std::uniform_int_distribution<> distrib(min, max); // Create uniform int dist between min and max (inclusive)
+
+    return distrib(gen); // Generate random number from the uniform int dist (inclusive)
+}
 
 
 int main()
 {
     int port = 8080;
+    int operations = 1000;
     std::string SERVER_IP = "127.0.0.1";
+    int successful_puts = 0;
+    int failed_puts = 0;
+    int successful_gets = 0;
+    int failed_gets = 0;
     
-    get_val(501, SERVER_IP, port);
-    get_val(502, SERVER_IP, port);
-    put_val(501, 500000, SERVER_IP, port);
-    get_val(501, SERVER_IP, port);
-    put_val(501, 500001, SERVER_IP, port);
-    get_val(501, SERVER_IP, port);
+    for(int i = 0; i < operations; i++){
+        int key = generateRandomInteger(INT8_MIN, INT8_MAX);
+        if (generateRandomInteger(1,5) == 1){
+            int val = generateRandomInteger(INT8_MIN, INT8_MAX);
+            put_val(key, val, SERVER_IP, port);
+        }
+        else{
+            if (get_val(key, SERVER_IP, port) == NULL){
+                failed_gets++;
+            }
+            else{
+                successful_gets++;
+            }
+        }
+    }
+    
+    // get_val(501, SERVER_IP, port);
+    // get_val(502, SERVER_IP, port);
+    // put_val(501, 500000, SERVER_IP, port);
+    // get_val(501, SERVER_IP, port);
+    // put_val(501, 500001, SERVER_IP, port);
+    // get_val(501, SERVER_IP, port);
+
+    std::cout << "Successful Gets: " << successful_gets << std::endl;
+    std::cout << "Failed Gets: " << failed_gets << std::endl;
 
     return 0;
 }
